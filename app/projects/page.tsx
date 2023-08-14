5ec20c2dbad62b740d490eedde66b9693b528669
Guardian <102593555+AardsEngel@users.noreply.github.com>
2023-08-14 22:41:38 +0200

    Update

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -13,7 +13,7 @@ export default function Projects() {
             Projecten
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Showcase your projects with a hero image (16 x 9)
+            De dingen die we leuk vinden
           </p>
         </div>
         <div className="container py-12">
