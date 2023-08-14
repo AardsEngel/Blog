00e0bb50770ad49471f3a654aeda436427f3105b
Guardian <102593555+AardsEngel@users.noreply.github.com>
2023-08-14 16:33:35 +0200

    Blog Update

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -4,7 +4,7 @@ import { slug } from 'github-slugger'
 import tagData from 'app/tag-data.json'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'Tags', description: 'Things I blog about' })
+export const metadata = genPageMetadata({ title: 'Tags', description: 'Dingen om over te bloggen' })
 
 export default async function Page() {
   const tagCounts = tagData as Record<string, number>
