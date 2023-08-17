b928d242aad5a351478c59941f908b4d62647bc3
Guardian <102593555+AardsEngel@users.noreply.github.com>
2023-08-17 11:57:14 +0200

    Blog Update

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,11 +1,13 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'ChatGPT',
+    description: `ChatGPT is een geavanceerd AI-model ontwikkeld door OpenAI. 
+    Het is getraind om mensachtige tekst te genereren en conversaties te voeren op basis van grote hoeveelheden tekstgegevens. 
+    Het model begrijpt en genereert tekst in natuurlijke taal, en kan worden gebruikt voor taken zoals het beantwoorden van vragen, 
+    het schrijven van tekst, het genereren van ideeën en het voeren van gesprekken met gebruikers. 
+    Het is gebaseerd op de GPT-3.5 architectuur en maakt gebruik van machine learning om contextuele en relevante antwoorden te produceren.`,
+    imgSrc: '/static/images/chatgpt.png',
+    href: 'https://chat.openai.com/',
   },
   {
     title: 'The Time Machine',
@@ -13,7 +15,7 @@ const projectsData = [
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
     imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    href: '/blog/ouderverstoting',
   },
 ]
 
