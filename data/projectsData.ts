1d21e63cb99b8191a216a1e007791ac9121b70b5
Gabriël <102593555+AardsEngel@users.noreply.github.com>
2024-03-20 20:04:23 +0100

    update

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -6,7 +6,7 @@ const projectsData = [
     Het systeem is ontwikkeld om een Dapp te zijn (Decentralised Application), die in verbinding staat met een cryptonetwerk. In dit geval, MINTME. Een social token community waar ik zonder investering toch al een paar extra centjes mee verdiend heb. En waar ik toch moeite in blijf steken ook.
     De voordelen zijn enorm lage "gasfees", dus om te ontwikkelen is dit ideaal. Gaat er wat mis begin je gewoon opnieuw, je verliest hooguit $0.0003 cent per transactie. Peanuts dus. Op het platform van mintme zelf waar er meer geld rondgaat is het wel wat duurder. Maar ontwikkelen doen we daar niet.
     Met mijn applicatie kan je tokens wisselen, omzetten naar mintme, tokens verdienen, staken en meer, maar dat is misschien wat ingewikkeld :) Het is alleszins een leuke bezighouding en wie weet komt er een dag dat ik hier ook echt geld mee kan maken, al is dit zeker geen prioriteit. `,
-    imgSrc: '/static/images/dogswap.png',
+    imgSrc: '/static/images/dogswap.jpeg',
     href: 'https://dogswap.online/',
   },
 ]
